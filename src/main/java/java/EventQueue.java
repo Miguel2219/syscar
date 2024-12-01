03c329a4a3cb0d725281778e70f97ae479577348
Miguel Angel <lmangelryl1512@gmail.com>
2024-12-01 15:59:14 -0500

    Interfaz sin terminar

---
+++ src/main/java/java/EventQueue.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java;
+
+/**
+ *
+ * @author ASUS
+ */
+public class EventQueue {
+
+    public static void invokeLater(Runnable runnable) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
